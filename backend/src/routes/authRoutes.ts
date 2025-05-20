816f5953bb2823191ff3233a1b308aa87a9e6a5e
StanislausJoel <stanislausjoel.fa@gmail.com>
2025-05-20 18:59:40 +0700

    add register route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, test } from "../controllers/authenticationController";
+import { login, logout, register, test } from "../controllers/authenticationController";
 import { authenticateToken } from "../middlewares/authMiddleware";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", login);
+authRouter.post("/register", register);
 authRouter.post("/logout", logout);
 authRouter.get("/test", authenticateToken, test);
 
