c791fe18772b3bda67c9de8bb9402145270a3dda
anrexus7 <ishigoemon878@gmail.com>
2025-05-23 22:33:28 +0700

    change target url in vite.config.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Development from "./pages/Development";
 import RegisterToko from "./pages/RegisterToko";
+import CreateItem from "./pages/addProduct";
 import "./styles/main.css";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/dev" element={<Development />} />
         <Route path="/register-toko" element={<RegisterToko />} />
+        <Route path="/add-product" element={<CreateItem />} />
         {/* 
         <Route path="/u/:username" element={
           <ProtectedRoute>
